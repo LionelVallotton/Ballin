85d1832498d8472e6ca820a993fc4037048dc992
LionelVallotton <lionel.vallotton@students.fhnw.ch>
2022-12-30 17:44:38 +0100

    implemented demo chart and building service

---
+++ src/Service/statisticService.js
@@ -0,0 +1,8 @@
+
+//export class statisticService{
+//    url = 'http://localhost:5000/statistics'
+//    allStatistics = [Statistic];
+//
+//    getAllStatistics():{
+//
+//    }
--- src/views/Statistics-Page.jsx
+++ src/views/Statistics-Page.jsx
@@ -2,14 +2,51 @@ import React from 'react'
 import { Container } from 'reactstrap'
 import '../css/statistics/Statistics-Page.css'
 
+import { useState } from "react";
+import { Data } from "../Utils/Data.js"
+import LineChart from "../Components/Statistics/lineChart";
+
 const StatisticPage = () => {
+    const [chartData, setChartData] = useState({
+        labels: Data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+ //       data: Data.map((data) => data.userGain),
+        data: ([5,6,7,8,9]),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      },
+      {
+        label: "Users Gained ",
+ //       data: Data.map((data) => data.userGain),
+        data: ([1,2,3,4,5]),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+      });
     return (
         <div className='statistic-page-layout'>
-            <div>
+            <div className='statistic-table-container'>
                 Statistics Table
             </div>
-            <div>
-                Statisticplots
+            <div className='statistic-plot-container'>
+                <LineChart chartData={chartData}/>
             </div>
         </div>
     )
